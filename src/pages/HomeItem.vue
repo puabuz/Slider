39799e658f95849fe6c7d303feb803226e52d1f3
Vitaly <naturbeer@mail.ru>
2022-12-25 21:49:55 +0300

    seeders creation

--- src/pages/HomeItem.vue
+++ src/pages/HomeItem.vue
@@ -7,54 +7,19 @@
       :body="item.discr" 
       :imgUrl="item.img">
       <img :src="item.img" :alt="item.title">
-    </CardItem>>
+    </CardItem>
   </div>
 </template>
 
 <script>
 
-import CardItem from '../components/UI/CardItem'
+import CardItem from '@/components/UI/CardItem'
+import items from "@/seeders/item.json"
 export default {
   components: { CardItem },
   data() {
     return {
-      items: [
-        {
-          id: 1,
-          title: 'Archer',
-          discr: 'Archer Archer Archer Archer Archer',
-          img: require('@/assets/image/archer.png'),
-          lvl: 4
-        },
-        {
-          id: 2,
-          title: 'Barbarian',
-          discr: 'Barbarian Barbarian Barbarian Barbarian',
-          img: require('@/assets/image/barbarian.png'),
-          lvl: 3
-        },
-        {
-          id: 3,
-          title: 'Giant',
-          discr: 'Giant Giant Giant Giant Giant Giant ',
-          img: require('@/assets/image/giant.png'),
-          lvl: 2
-        },
-        {
-          id: 3,
-          title: 'Goblin',
-          discr: 'Goblin Goblin Goblin Goblin Goblin Goblin ',
-          img: require('@/assets/image/goblin.png'),
-          lvl: 2
-        },
-        {
-          id: 3,
-          title: 'Wizard',
-          discr: 'Wizard Wizard Wizard Wizard Wizard Wizard ',
-          img: require('@/assets/image/wizard.png'),
-          lvl: 2
-        },
-      ]
+      items
     }
   }
 }
